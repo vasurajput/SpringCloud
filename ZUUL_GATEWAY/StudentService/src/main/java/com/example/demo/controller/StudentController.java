f2e5713be3b87b5258b62eb2c01473724720e4fb
vasu <vashurajput005@gmail.com>
2020-02-26 21:15:54 +0530

    Zull Gateway Routing

---
+++ ZUUL_GATEWAY/StudentService/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+	@GetMapping("/student")
+	public String studentEndPoint() {
+		return "Nice Try This is Student Service";
+	}
+}
