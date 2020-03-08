1295b7a47f365427d7892117e32170928f26612c
vasu <vashurajput005@gmail.com>
2020-03-08 20:50:29 +0530

    H2 database example

---
+++ SpringBootH2Database/src/main/java/com/example/demo/SpringBootH2DatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2DatabaseApplication.class, args);
+	}
+
+}
---
+++ SpringBootH2Database/src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,58 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "studentId")
+	private long id;
+
+	private String name;
+
+	private String emailId;
+
+	public Student() {
+		super();
+	}
+
+	public Student(long id, String name, String emailId) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.emailId = emailId;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+}
